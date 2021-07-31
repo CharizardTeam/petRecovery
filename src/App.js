6331c5da21cebd2126b8f2428a2ca72c0b7c4066
jenpoole <contact.vandepool@gmail.com>
2021-07-31 16:25:09 -0400

    layout - header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Switch, Route } from "react-router-dom";
 
 // COMPONENTS
+import Header from "./components/Header";
 import LandingPage from "./pages/LandingPage";
 import LostPet from "./pages/LostPet";
 import FoundPet from "./pages/FoundPet";
 
 export default function App() {
   return (
-    <Switch>
-      <Route path="/lost-my-pet" component={LostPet}></Route>
-      <Route path="/found-a-pet" component={FoundPet}></Route>
-      <Route exact path="/" component={LandingPage}></Route>
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/lost-my-pet" component={LostPet}></Route>
+        <Route path="/found-a-pet" component={FoundPet}></Route>
+        <Route exact path="/" component={LandingPage}></Route>
+      </Switch>
+    </div>
   );
 }
