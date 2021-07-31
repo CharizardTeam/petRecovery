7c7ddfe69e7b854c5c3b690899932867bf6ce053
jenpoole <contact.vandepool@gmail.com>
2021-07-31 12:42:48 -0400

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+
+  
+  module.exports = {
+    // ...
+    theme: {
+      extend: {
+        colors: {
+          teal: colors.teal,
+          cyan: colors.cyan,
+        }
+      }
+    },
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/aspect-ratio'),
+    ]
+  }
+  ```
+*/
