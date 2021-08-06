6def028d17df3e7c447a78da6e53e686dee575a4
jenpoole <contact.vandepool@gmail.com>
2021-08-05 21:42:21 -0400

    add links to cards

--- src/components/LostOrFound/LostOrFound.js
+++ src/components/LostOrFound/LostOrFound.js
@@ -1,36 +1,39 @@
-import React from 'react';
-import '../LostOrFound/lostorfound.css'
-import dog from '../../images/02-bull-dog.png';
-import cat from '../../images/17-persian.png';
-
+import React from "react";
+import "../LostOrFound/lostorfound.css";
+import dog from "../../images/02-bull-dog.png";
+import cat from "../../images/17-persian.png";
+import { Link } from "react-router-dom";
 
 export default function LostOrFound() {
-
   return (
-//     <Container className='Button-Container'>
-// <Cardbox/>
-// <Cardbox/>
-//     </Container>
-<div class="container">
-  <button class="card">
-    <div class="boxes">
-      <div class="content">
-        <h2>Lost</h2>
-        <h3>I Lost My Pet</h3>
-         <img src={dog} alt="dog" class="img-responsive" /> 
-      </div>
-    </div>
-  </button>
+    //     <Container className='Button-Container'>
+    // <Cardbox/>
+    // <Cardbox/>
+    //     </Container>
+    <div class="container">
+      <Link to={"/lost-pet"}>
+        <div class="card">
+          <div class="boxes">
+            <div class="content">
+              <h2>Lost</h2>
+              <h3>I Lost My Pet</h3>
+              <img src={dog} alt="dog" class="img-responsive" />
+            </div>
+          </div>
+        </div>
+      </Link>
 
-  <div class="card">
-    <div class="boxes">
-      <div class="content">
-        <h2>Found</h2>
-        <h3>I Found a Pet</h3>
-        <img src={cat} alt="dog" class="img-responsive" /> 
-      </div>
+      <Link to={"/found-pet"}>
+        <div class="card">
+          <div class="boxes">
+            <div class="content">
+              <h2>Found</h2>
+              <h3>I Found a Pet</h3>
+              <img src={cat} alt="dog" class="img-responsive" />
+            </div>
+          </div>
+        </div>
+      </Link>
     </div>
-  </div>
-</div>
-  )
+  );
 }
